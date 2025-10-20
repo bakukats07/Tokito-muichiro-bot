ca0f51455033bf1e09acbad158de03b78ddc6929
Toki_mui7798 <crisvegar2@gmail.com>
2025-10-19 20:39:08 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -41,9 +41,9 @@ global.catalogo = fs.readFileSync('./lib/catalogo.jpg')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.group = 'https://chat.whatsapp.com/HaKf6ezcwdbGzmH782eBal'
-global.community = 'https://chat.whatsapp.com/G0kXqsteJFU74yrLtg79o6'
-global.channel = 'https://whatsapp.com/channel/0029Vb64nWqLo4hb8cuxe23n'
+global.group = 'https://chat.whatsapp.com/E7foYUiRVDQ4FSRwolDNzG?mode=wwt'
+global.community = 'https://chat.whatsapp.com/ECZeU9ipYKlIeTxzdjvtgW?mode=wwt'
+global.channel = 'https://whatsapp.com/channel/0029VbBFWP0Lo4hgc1cjlC0M'
 global.github = 'https://bakukats07/Tokito-muichiro-bot'
 global.gmail = 'thekingdestroy507@gmail.com'
 global.ch = {
